58efce77df45f49ba1750140abde108e791b3620
kokorolx <kokoro.lehoang@gmail.com>
2024-06-08 01:13:10 +0700

    fix: logstash

--- docker-swarm-scripts/update_node_label.sh
+++ docker-swarm-scripts/update_node_label.sh
@@ -10,17 +10,17 @@ fi
 # Specify the label name and base value
 label_name="DATACENTER_ID"
 
-docker node update sudox-01 --label-add "$label_name=0";
-docker node update azure-01 --label-add "$label_name=1";
-docker node update aws-01 --label-add "$label_name=2";
+docker node update docker-desktop --label-add "$label_name=0";
+docker node update docker-desktop --label-add "$label_name=1";
+docker node update docker-desktop --label-add "$label_name=2";
 
 # ELK stack
-docker node update aws-01 --label-add logging_e=true;
-docker node update sudox-01 --label-add logging_l=true;
-docker node update azure-01 --label-add logging_k=true;
+docker node update docker-desktop --label-add logging_e=true;
+docker node update docker-desktop --label-add logging_l=true;
+docker node update docker-desktop --label-add logging_k=true;
 
 # Database
-docker node update azure-01 --label-add database=true;
+docker node update docker-desktop --label-add database=true;
 
 # web server
 # docker node update azure-01 --label-add web=true;
